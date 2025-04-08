8b9fb9773a05403468abd6e7f59737c29e0daf34
dbingabire05@gmail.com <dbingabire05@gmail.com>
2025-04-08 22:12:46 +0200

    created employees table

---
+++ table_creation.sql
@@ -0,0 +1,8 @@
+-- Create a sample dataset(Employess Table)
+CREATE TABLE Employees (
+EmployeeID NUMBER PRIMARY KEY,
+EmployeeName VARCHAR(100),
+Department VARCHAR(100),
+Salary NUMBER,
+JoinDate DATE
+);
