d02a342e36ba0b39efb1bdbe695d2dd1fc21bc05
Gisele umutoni <umutonigisele339@gmail.com>
2025-04-11 16:36:57 +0200

    Added window function using LAG(),LEAD(),RANK() and DENSE_RANK()

--- table_creation.sql
+++ table_creation.sql
@@ -7,7 +7,27 @@ Salary NUMBER,
 JoinDate DATE
 );
 
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (1, 'Alice', 'IT', 9000, TO_DATE('2021-03-15', 'YYYY-MM-DD'));
 
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (2, 'Bob', 'IT', 8500, TO_DATE('2020-07-10', 'YYYY-MM-DD'));
+
+
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (3, 'Charlie', 'IT', 8500, TO_DATE('2021-06-20', 'YYYY-MM-DD'));
+
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (4, 'David', 'IT', 8000, TO_DATE('2019-09-25', 'YYYY-MM-DD'));
+
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (5, 'Eve', 'HR', 7500, TO_DATE('2022-01-05', 'YYYY-MM-DD'));
+
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (6, 'Frank', 'HR', 7200, TO_DATE('2021-08-14', 'YYYY-MM-DD'));
+
+INSERT INTO Employees (EmployeeID, EmployeeName, Department, Salary, JoinDate)
+VALUES (7, 'Grace', 'HR', 7000, TO_DATE('2020-11-30', 'YYYY-MM-DD'));
 
 SELECT * FROM Employees;
 
@@ -25,4 +45,15 @@ SELECT
         WHEN Salary < LAG(Salary) OVER (PARTITION BY Department ORDER BY Salary) THEN 'LOWER'
         ELSE 'EQUAL' 
     END AS ComparisonWithPrev
+FROM Employees;
+
+-- 2. Ranking Data within a Category
+-- Using RANK() and DENSE_RANK() to rank employees by salary within each department
+SELECT 
+    EmployeeID, 
+    EmployeeName, 
+    Department, 
+    Salary,
+    RANK() OVER (PARTITION BY Department ORDER BY Salary DESC) AS Rank,
+    DENSE_RANK() OVER (PARTITION BY Department ORDER BY Salary DESC) AS DenseRank
 FROM Employees;
